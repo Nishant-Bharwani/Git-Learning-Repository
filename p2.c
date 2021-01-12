9d85e2fddd139c5988515529defa4632e00de0e4
Nishant Bharwani <nbtaylor1031@gmail.com>
2021-01-12 18:18:23 +0530

    p2.c added

---
+++ p2.c
@@ -0,0 +1,32 @@
+// 27 November 2020
+// Program to print the pattern2
+
+/*
+
+* 
+* *
+* * *
+* * * *
+
+*/
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Enter pattern size: ");
+    scanf("%d",&n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+
+        printf("\n");
+        
+    }
+    
+    
+}
