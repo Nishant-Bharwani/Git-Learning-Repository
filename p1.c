58decf716dbf77542857a8c2793446b32449bd51
Nishant Bharwani <nbtaylor1031@gmail.com>
2021-01-12 18:08:01 +0530

    initial commit

---
+++ p1.c
@@ -0,0 +1,29 @@
+// 27 November 2020
+// Program to print the pattern1
+
+
+/*
+* * * * * 
+* * * * *
+* * * * * 
+* * * * *
+* * * * *
+*/
+
+#include <stdio.h>
+void main()
+{
+    int n;
+    printf("Enter pattern size: ");
+    scanf("%d",&n);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        
+    }
+    
+}
